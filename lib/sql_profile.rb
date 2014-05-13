7f4675125c8ebf468a10555ee773ff6035241786
winton <mail@wintoni.us>
2014-05-12 23:59:05 -0700

    Storing EXPLAINs

--- lib/sql_profile.rb
+++ lib/sql_profile.rb
@@ -1,6 +1,10 @@
 $:.unshift File.dirname(__FILE__)
 
 module SqlProfile
+
+  mattr_accessor :controller
+  mattr_accessor :data
+  mattr_accessor :segment
   
   def self.append_features(klass)
     super
@@ -12,13 +16,40 @@ module SqlProfile
       end
     end
   end
-  
-  def log_info_with_trace(sql, name, runtime)
-    log_info_without_trace(sql, name, runtime)
-    
-    return unless @logger and @logger.debug?
-    return if / Columns$/ =~ name
 
-    @logger.info "!!! #{sql}"
+  def self.around_filter(controller, segment=nil)
+    SqlProfile.controller = controller
+    SqlProfile.segment    = controller
+    yield
+  ensure
+    SqlProfile.controller = nil
+    SqlProfile.segment    = nil
+
+    Rails.logger.info "!"*50
+    Rails.logger.info SqlProfile.data.pretty_inspect
+  end
+  
+  def log_info_with_profile(sql, name, runtime)
+    log_info_without_profile(sql, name, runtime)
+
+    return unless controller = SqlProfile.controller    
+    return unless @logger and @logger.debug?
+
+    return if / Columns$/     =~ name
+    return if /Mysql::Error/  =~ sql
+    return if /^EXPLAIN/      =~ sql
+
+    results  = ActiveRecord::Base.connection.execute("EXPLAIN #{sql}")
+    path     = controller.request.path
+    explains = [] 
+
+    while explain = results.fetch_hash
+      explain.delete('rows')
+      explains << explain
+    end
+    
+    SqlProfile.data       ||= {}
+    SqlProfile.data[path] ||= []
+    SqlProfile.data[path]  << explains
   end
 end
